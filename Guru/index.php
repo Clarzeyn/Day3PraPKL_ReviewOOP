3aebe9a2d635dc478be974377cb5e60350dceb60
Clarzeyn <Wildan.mnf05@gmail.com>
2022-07-07 14:00:25 +0700

    Review Materi OOP 2

--- Guru/index.php
+++ Guru/index.php
@@ -50,7 +50,7 @@
                         <th scope="col">NIP</th>
                         <th scope="col">Nama Guru</th>
                         <th scope="col">Alamat</th>
-                        <th scope="col">Action</th>
+                        <th scope="col">Aksi</th>
                         </tr>
                     </thead>
                     
